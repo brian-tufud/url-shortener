139e67c5e64a67865e8663d6511d423056ee6498
Brian <brian.cabrera@tufud.co>
2024-04-11 19:47:57 -0300

    [UTILS] created Constants.java for storing constants

---
+++ src/main/java/com/urlshortener/api/utils/Constants.java
@@ -0,0 +1,19 @@
+package com.urlshortener.api.utils;
+
+public class Constants {
+    public static final String SHORT_URL_PREFIX = "http://localhost:8080/";
+
+    public static final String[] IP_HEADER_CANDIDATES = {
+        "X-Forwarded-For",
+        "Proxy-Client-IP",
+        "WL-Proxy-Client-IP",
+        "HTTP_X_FORWARDED_FOR",
+        "HTTP_X_FORWARDED",
+        "HTTP_X_CLUSTER_CLIENT_IP",
+        "HTTP_CLIENT_IP",
+        "HTTP_FORWARDED_FOR",
+        "HTTP_FORWARDED",
+        "HTTP_VIA",
+        "REMOTE_ADDR"
+    };
+}
