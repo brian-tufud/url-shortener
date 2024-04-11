addf65e031b244190ca77a1534e40454c3fe6bd2
Brian <brian.cabrera@tufud.co>
2024-04-11 01:31:21 -0300

    [URL SERVICE] created generateRandomSuffix method

--- src/main/java/com/urlshortener/api/service/UrlService.java
+++ src/main/java/com/urlshortener/api/service/UrlService.java
@@ -1,7 +1,11 @@
 package com.urlshortener.api.service;
 
 import com.urlshortener.api.repository.UrlRepositoryService;
-import com.urlshortener.api.utils.utilsService;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,7 +13,17 @@ import org.springframework.stereotype.Service;
 public class UrlService {
     
     @Autowired
-    private UrlRepositoryService  userRepositoryService;
-   
-    
+    private UrlRepositoryService userRepositoryService;
+
+    public static String generateRandomSuffix(int length) {
+        String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder suffix = new StringBuilder();
+        Random random = new Random();
+        
+        for (int i = 0; i < length; i++) {
+            suffix.append(alphabet.charAt(random.nextInt(alphabet.length())));
+        }
+        return suffix.toString();
+    }
+
 }
