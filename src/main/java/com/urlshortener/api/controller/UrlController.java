7cb3d578e6d708009b530137c998ac7060454417
Brian <brian.cabrera@tufud.co>
2024-04-11 02:54:52 -0300

    [URL CONTROLLER] created shortenURL endpoint with POST method in URLController.java

--- src/main/java/com/urlshortener/api/controller/UrlController.java
+++ src/main/java/com/urlshortener/api/controller/UrlController.java
@@ -1,7 +1,12 @@
 package com.urlshortener.api.controller;
 
 import com.urlshortener.api.service.UrlService;
-import com.urlshortener.api.utils.utilsService;
+import com.urlshortener.api.utils.UtilsService;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,12 +15,21 @@ import org.springframework.web.bind.annotation.*;
 public class UrlController {
 
     private final UrlService urlService;
-    private final utilsService utilsService;
+    private final UtilsService utilsService;
 
-    public UrlController(UrlService urlService, utilsService utilsService) {
+    public UrlController(UrlService urlService, UtilsService utilsService) {
         super();
         this.urlService = urlService;
         this.utilsService = utilsService;
+    }
+
+    @PostMapping("/shorten")
+    public ResponseEntity<String> shortenURL(HttpServletRequest request) throws Exception {
+
+        String cart = urlService.shortenURL(request.getParameter("url"));
+
+        HttpHeaders responseHeaders = utilsService.getResponseHeaders();
+        return ResponseEntity.ok().headers(responseHeaders).body(cart);
     }
     
 }
