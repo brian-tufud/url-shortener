aad73a3ee58ff420e635100231685fadab70b4bb
Brian <brian.cabrera@tufud.co>
2024-04-12 05:10:08 -0300

    [UTILS] added STATISTICS_QUEUE constant to Constants.java

--- src/main/java/com/urlshortener/api/utils/Constants.java
+++ src/main/java/com/urlshortener/api/utils/Constants.java
@@ -1,12 +1,14 @@
 package com.urlshortener.api.utils;
 
 public class Constants {
-    public static final String SHORT_URL_PREFIX = "https://be67-2800-a4-22a5-c200-3480-7248-813b-860e.ngrok-free.app/";
+    public static final String SHORT_URL_PREFIX = "https://7f52-2800-a4-22a5-c200-3480-7248-813b-860e.ngrok-free.app/";
 
     public static final String IP_STACK_URL = "http://api.ipstack.com/";
 
     //TODO - borrar esto de acá y ponerlo como env variable en la instancia al momento del deploy
     public static final String IP_STACK_ACCESS_KEY = "f6ae78026243789416ef752878bf75c9";
+
+    public static final String STATISTICS_QUEUE = "statistics_queue";
 
     public static final String[] IP_HEADER_CANDIDATES = {
         "X-Forwarded-For",
