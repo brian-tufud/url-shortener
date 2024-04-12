5e8cd4a0b24899815d1286650021907fc8e3faee
Brian <brian.cabrera@tufud.co>
2024-04-11 22:03:27 -0300

    [UTILS] Added IP_STACK_URL and IP_STACK_ACCESS_KEY (last one for testing purposes) to Constants.java

--- src/main/java/com/urlshortener/api/utils/Constants.java
+++ src/main/java/com/urlshortener/api/utils/Constants.java
@@ -1,7 +1,12 @@
 package com.urlshortener.api.utils;
 
 public class Constants {
-    public static final String SHORT_URL_PREFIX = "http://localhost:8080/";
+    public static final String SHORT_URL_PREFIX = "https://be67-2800-a4-22a5-c200-3480-7248-813b-860e.ngrok-free.app/";
+
+    public static final String IP_STACK_URL = "http://api.ipstack.com/";
+
+    //TODO - borrar esto de acá y ponerlo como env variable en la instancia al momento del deploy
+    public static final String IP_STACK_ACCESS_KEY = "f6ae78026243789416ef752878bf75c9";
 
     public static final String[] IP_HEADER_CANDIDATES = {
         "X-Forwarded-For",
