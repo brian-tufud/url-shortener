3f23b150fc7213aec939ae6ab3478dbbb2b796ff
Brian <brian.cabrera@tufud.co>
2024-04-12 22:25:22 -0300

    [URL SERVICE] shortenUrl method refactor and DB implementation using UrlRepository

--- src/main/java/com/urlshortener/api/service/UrlService.java
+++ src/main/java/com/urlshortener/api/service/UrlService.java
@@ -1,6 +1,6 @@
 package com.urlshortener.api.service;
 
-import com.urlshortener.api.repository.UrlRepositoryService;
+import com.urlshortener.api.repository.UrlRepository;
 import com.urlshortener.api.utils.Constants;
 
 import java.math.BigInteger;
@@ -15,30 +15,27 @@ import org.springframework.stereotype.Service;
 public class UrlService {
     
     @Autowired
-    private UrlRepositoryService urlRepositoryService;
+    private UrlRepository urlRepository;
 
     public String getLongURL(String shortURL) {
-        return "https://articulo.mercadolibre.com.uy/MLU-460530019-carpa-6-personas-colchon-inflable-de-2-plazas-inflador--_JM#reco_item_pos=2&reco_backend=item_decorator&reco_backend_type=function&reco_client=home_items-decorator-legacy&reco_id=583bf3a0-af13-4478-8184-239917f20781&c_id=/home/navigation-trends-recommendations/element&c_uid=994aa749-bccd-43cd-8c0a-af26a4c77820&da_id=navigation_trend&da_position=2&id_origin=/home/dynamic_access&da_sort_algorithm=ranker";
+        return urlRepository.getLongUrl(shortURL);
     }
 
     public String shortenURL(String longURL) {
+        String fragment = generateFragment(longURL);
+
+        while (urlRepository.checkIfShortUrlExists(fragment)) {
+            fragment = generateFragment(longURL);
+        }
+        
+        return save(fragment, longURL);
+    }
+
+    private String save(String fragment, String longURL) {
         try {
-            String randomSuffix = generateRandomSuffix(6);
-
-            String longURLWithSuffix = longURL + randomSuffix;
-
-            MessageDigest md = MessageDigest.getInstance("SHA-512");
-            byte[] digest = md.digest(longURLWithSuffix.getBytes());
-            BigInteger hashInt = new BigInteger(1, digest);
-            
-            String base62Fragment = encodeBase62(hashInt);
-            
-            String shortFragment = base62Fragment.substring(0, 6);
-            
-            String shortURL = Constants.SHORT_URL_PREFIX + shortFragment;
-
-            return shortURL;
-        } catch (NoSuchAlgorithmException e) {
+            urlRepository.insert(fragment, longURL);
+            return Constants.SHORT_URL_PREFIX + fragment;
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
@@ -54,6 +51,30 @@ public class UrlService {
         }
         return suffix.toString();
     }
+
+    private String generateFragment(String longURL) {
+        String randomSuffix = generateRandomSuffix(6);
+
+        String longURLWithSuffix = longURL + randomSuffix;
+
+        BigInteger urlHash = generateUrlHashInt(longURLWithSuffix);
+        
+        String base62Fragment = encodeBase62(urlHash);
+
+        return base62Fragment.substring(0, 6);
+    }
+
+    private BigInteger generateUrlHashInt(String longURL) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            byte[] digest = md.digest(longURL.getBytes());
+            BigInteger hashInt = new BigInteger(1, digest);
+            return hashInt;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }   
 
     private String encodeBase62(BigInteger num) {
         String alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
