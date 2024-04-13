3693e4086f13efd77fcc0fa52b650b74166b4403
Brian <brian.cabrera@tufud.co>
2024-04-12 21:16:33 -0300

    [UTILS] added database constants to Constants.java

--- src/main/java/com/urlshortener/api/utils/Constants.java
+++ src/main/java/com/urlshortener/api/utils/Constants.java
@@ -10,6 +10,38 @@ public class Constants {
 
     public static final String STATISTICS_QUEUE = "statistics_queue";
 
+    public static final Integer ZERO_NUMERIC_VALUE = 48;
+
+    public static final Integer NINE_NUMERIC_VALUE = 57;
+
+    public static final Integer LOWERCASE_A_VALUE = 97;
+
+    public static final Integer LOWERCASE_M_VALUE = 109;
+
+    public static final Integer LOWERCASE_N_VALUE = 110;
+
+    public static final Integer LOWERCASE_Z_VALUE = 122;
+
+    public static final Integer UPPERCASE_A_VALUE = 65;
+
+    public static final Integer UPPERCASE_M_VALUE = 77;
+
+    public static final Integer UPPERCASE_N_VALUE = 78;
+
+    public static final Integer UPPERCASE_Z_VALUE = 90;
+
+    private static final String DB_TABLE_PREFIX = "short_url_shard_";
+    
+    public static final String DB_SHARD_A = DB_TABLE_PREFIX + "a";
+
+    public static final String DB_SHARD_B = DB_TABLE_PREFIX + "b";
+
+    public static final String DB_SHARD_C = DB_TABLE_PREFIX + "c";
+
+    public static final String DB_SHARD_D = DB_TABLE_PREFIX + "d";
+
+    public static final String DB_SHARD_E = DB_TABLE_PREFIX + "e";
+
     public static final String[] IP_HEADER_CANDIDATES = {
         "X-Forwarded-For",
         "Proxy-Client-IP",
