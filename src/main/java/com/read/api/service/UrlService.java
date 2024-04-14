d9c8b59c2a84c500ae1155ca5ec00cb537815b2f
Brian <brian.cabrera@tufud.co>
2024-04-14 19:17:08 -0300

    [SERVICE] modified getLongURL in UrlService

--- src/main/java/com/read/api/service/UrlService.java
+++ src/main/java/com/read/api/service/UrlService.java
@@ -3,16 +3,23 @@ package com.read.api.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.read.api.dto.LongURLDto;
 import com.read.api.repository.UrlRepository;
+import com.read.api.utils.UtilsService;
 
 @Service
 public class UrlService {
+
+    @Autowired
+    private UtilsService utilsService;
     
     @Autowired
     private UrlRepository urlRepository;
 
-    public String getLongURL(String shortURL) {
-        return urlRepository.getLongUrl(shortURL);
+    public LongURLDto getLongURL(String shortURL) {
+        String longUrl = urlRepository.getLongUrl(shortURL);
+
+        return utilsService.getLongURLDto(longUrl);
     }
 
 }
